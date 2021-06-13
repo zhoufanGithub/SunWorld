59c49f567ff411a8d94d5fd55cabb68487e6b105
zhoufanGithub <544192676@qq.com>
2021-06-13 13:53:41 +0800

    添加log日志文件

---
+++ baselibrary/src/main/java/com/steven/baselibrary/Util/LogUtil.java
@@ -0,0 +1,55 @@
+package com.steven.baselibrary.Util;
+
+import android.util.Log;
+
+/**
+ * author: zhoufan
+ * data: 2021/6/13 11:18
+ * content:日志工具打印类
+ */
+public class LogUtil {
+
+    private static final int VERBOSE = 1;
+
+    private static final int DEBUG = 2;
+
+    private static final int INFO = 3;
+
+    private static final int WARN = 4;
+
+    private static final int ERROR = 5;
+
+    private static final int NOTHING = 6;
+
+    private static int level = VERBOSE;
+
+    public static void v(String tag, String msg) {
+        if (level <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (level <= DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (level <= INFO) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg) {
+        if (level <= WARN) {
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (level <= ERROR) {
+            Log.e(tag, msg);
+        }
+    }
+}
